70ac58f24a97a2c9cd6d0b2fc1db1c3d57143d8e
gaju91 <gajanand742@gmail.com>
2025-10-23 06:38:16 +0530

    Moving Directory

--- app-v5/src/config/i18n.config.ts
+++ app-v5/src/config/i18n.config.ts
@@ -4,7 +4,7 @@
  */
 
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import { initReactI18next } from 'node_modules/react-i18next';
 import brandConfig from './brand.config';
 
 // Import translations
--- app-v5/src/core/providers/LanguageProvider.tsx
+++ app-v5/src/core/providers/LanguageProvider.tsx
@@ -4,7 +4,7 @@
  */
 
 import { createContext, useEffect, useState, type ReactNode } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'node_modules/react-i18next';
 import type { Language, LanguageContextType } from '../types';
 import brandConfig from '@/config/brand.config';
 
